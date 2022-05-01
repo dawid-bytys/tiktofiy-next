a417584e0d742010e5cebffd14dcdc4813f5d894
salvia-dev <salviadev@protonmail.com>
2022-05-01 20:52:56 +0200

    feat: added logic to settings page

--- components/molecules/MainSettings.tsx
+++ components/molecules/MainSettings.tsx
@@ -1,19 +1,54 @@
 import { motion } from 'framer-motion';
+import React from 'react';
 import { useFadeLeftTransition } from '../../hooks/useFadeLeftTransition';
+import { useSettings } from '../../hooks/useSettings';
+import type { SettingsKeys } from '../../utils/types';
 
 const INPUT_STYLES = 'bg-input p-3 rounded-xl text-sm text-center';
 
 export const MainSettings = () => {
   const motionProps = useFadeLeftTransition();
+  const { setSettings } = useSettings();
+
+  const handleChange =
+    (key: SettingsKeys) =>
+    (e: React.ChangeEvent<HTMLInputElement> | React.ClipboardEvent<HTMLInputElement>) => {
+      if (key === 'shazamApiKey') {
+        return setSettings(key, e.currentTarget.value);
+      }
+
+      setSettings(key, +e.currentTarget.value);
+    };
 
   return (
     <motion.main {...motionProps} className="flex justify-center flex-1 p-8 sm:py-16 sm:px-0">
       <div className="flex flex-col align-center gap-5 w-full sm:w-72">
         <div className="text-center">shazam api key</div>
-        <input type="password" placeholder="paste the key..." className={INPUT_STYLES} />
+        <input
+          type="password"
+          name="shazamApiKey"
+          placeholder="paste the key..."
+          className={INPUT_STYLES}
+          onChange={handleChange('shazamApiKey')}
+          onPaste={handleChange('shazamApiKey')}
+        />
         <div className="text-center">settings</div>
-        <input type="number" placeholder="from" className={INPUT_STYLES} />
-        <input type="number" placeholder="to" className={INPUT_STYLES} />
+        <input
+          type="number"
+          name="start"
+          placeholder="from"
+          className={INPUT_STYLES}
+          onChange={handleChange('start')}
+          onPaste={handleChange('start')}
+        />
+        <input
+          type="number"
+          name="end"
+          placeholder="to"
+          className={INPUT_STYLES}
+          onChange={handleChange('end')}
+          onPaste={handleChange('end')}
+        />
       </div>
     </motion.main>
   );
