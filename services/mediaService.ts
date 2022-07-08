fd39cbff323657a9fffc85adcd08effc9b7f1f53
salvia-dev <salviadev@protonmail.com>
2022-07-08 20:50:11 +0200

    fix: mediaService path

--- services/mediaService.ts
+++ services/mediaService.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import { ClearMediaError } from 'utils/errors';
+import { returnPath } from 'utils/utils';
 
 export const clearMedia = async (filenames: string[]) => {
-	const unlinks = filenames.map(fs.promises.unlink);
+	const unlinks = filenames.map(filename => fs.promises.unlink(returnPath(filename)));
 
 	try {
 		await Promise.all(unlinks);
