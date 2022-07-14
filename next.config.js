a6e838e0820d71dcfbb4b62bdda078eb064017e2
salvia-dev <salviadev@protonmail.com>
2022-07-14 22:05:17 +0200

    fix: domain and cors

--- next.config.js
+++ next.config.js
@@ -22,11 +22,11 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: 'tiktofiy.com',
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
             value:
