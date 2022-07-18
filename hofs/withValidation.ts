fd800762aeddc6c461520a4702b86bdce9174811
salvia-dev <salviadev@protonmail.com>
2022-07-18 13:15:03 +0200

    fix/chore: fixed cors, changed workflows

--- hofs/withValidation.ts
+++ hofs/withValidation.ts
@@ -12,19 +12,6 @@ export const withValidation = <T extends SomeSchema | undefined = undefined>(
     handler: (req: V, res: NextApiResponse) => Promise<unknown>,
   ) => {
     return async (req: V, res: NextApiResponse) => {
-      // enable CORS
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-      );
-
-      if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-      }
-
       try {
         if (!methods.includes(req.method as HTTPMethod)) {
           throw new InvalidHTTPMethodError(
