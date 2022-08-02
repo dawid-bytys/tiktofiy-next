428d043afaffdc92421c3ba1ace3e94eeb933ffb
salvia-dev <salviadev@protonmail.com>
2022-08-02 19:36:43 +0200

    refactor: useFilteredThemes hook (add useMemo)

--- hooks/useFilteredThemes.ts
+++ hooks/useFilteredThemes.ts
@@ -1,14 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useMemo } from 'react';
 import { useDebounce } from './useDebounce';
 
 export const useFilteredThemes = (themeCollection: string[], filter: string, debounce?: number) => {
-  const [themes, setThemes] = useState(themeCollection);
   const debouncedFilter = useDebounce(filter, debounce);
 
-  useEffect(() => {
-    const filteredThemes = themeCollection.filter(theme => theme.includes(debouncedFilter));
-    setThemes(filteredThemes);
-  }, [filter, debouncedFilter, themeCollection]);
+  const filteredThemes = useMemo(() => {
+    return themeCollection.filter(theme => theme.includes(debouncedFilter));
+  }, [debouncedFilter]);
 
-  return themes;
+  return filteredThemes;
 };
